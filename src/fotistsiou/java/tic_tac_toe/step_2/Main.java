dc2b6a37c59c3c88a3d01449da5091b73f36ca5f
fotistsiou <fotis.tsioumas@outlook.com>
2024-11-14 10:23:23 +0200

    Change README.md

--- src/fotistsiou/java/tic_tac_toe/step_2/Main.java
+++ src/fotistsiou/java/tic_tac_toe/step_2/Main.java
@@ -3,13 +3,13 @@ package fotistsiou.java.tic_tac_toe.step_2;
 import java.util.Scanner;
 
 /**
- * The user is the gamemaster
- * --------------------------
+ * The user is the gamemaster!
+ * ---------------------------
  * Description
  * Our program should be able to display the grid at all stages of the game.
  * Now we’re going to write a program that allows the user to enter a string representing the game state and correctly
  * prints the 3x3 game grid based on this input. We’ll also add some boundaries around the game grid.
- * --------------------------
+ * ---------------------------
  * Objectives
  * In this stage, you will write a program that:
  * 1. Reads a string of 9 symbols from the input and displays them to the user in a 3x3 grid.
